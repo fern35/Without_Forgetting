b79602f3ab036e3feedf2e548df40066664d22cc
ZHANGyuan <fern35121321@outlook.com>
2019-01-13 18:01:06 +0100

    change test epoch of Resnet10Att

--- config/miniImageNet_ResNet10CosineClassifierGenWeightAttN1.py
+++ config/miniImageNet_ResNet10CosineClassifierGenWeightAttN1.py
@@ -12,7 +12,7 @@ data_train_opt['nExemplars'] = nExemplars
 data_train_opt['nTestNovel'] = nKnovel * 3
 data_train_opt['nTestBase'] = nKnovel * 3
 data_train_opt['batch_size'] = 32
-data_train_opt['epoch_size'] = data_train_opt['batch_size'] * 250
+data_train_opt['epoch_size'] = data_train_opt['batch_size'] * 500
 
 data_test_opt = {}
 data_test_opt['nKnovel'] = nKnovel
