7eec853f259cebd8fd5c8aa6abee908669b8b20a
ZHANGyuan <fern35121321@outlook.com>
2019-01-14 02:57:07 +0100

    test experiments of CUB, change nTestBase to 0

--- config/cub_ResNet10CosineClassifier.py
+++ config/cub_ResNet10CosineClassifier.py
@@ -30,10 +30,10 @@ config['data_test_opt'] = data_test_opt
 config['max_num_epochs'] = 4
 
 networks = {}
-net_optim_paramsF = {'optim_type': 'sgd', 'lr': 0.1, 'momentum':0.9, 'weight_decay': 5e-4, 'nesterov': True, 'LUT_lr':[(20, 0.05),(40, 0.006),(50, 0.0012),(60, 0.00024)]}
+net_optim_paramsF = {'optim_type': 'sgd', 'lr': 0.1, 'momentum':0.9, 'weight_decay': 5e-4, 'nesterov': True, 'LUT_lr':[(20, 0.01),(40, 0.006),(50, 0.0012),(60, 0.00024)]}
 networks['feat_model'] = {'def_file': 'architectures/ResNetFeat.py', 'pretrained': None, 'opt': {'userelu': False, 'restype': 'ResNet10'},  'optim_params': net_optim_paramsF}
 
-net_optim_paramsC = {'optim_type': 'sgd', 'lr': 0.1, 'momentum':0.9, 'weight_decay': 5e-4, 'nesterov': True, 'LUT_lr':[(20, 0.05),(40, 0.006),(50, 0.0012),(60, 0.00024)]}
+net_optim_paramsC = {'optim_type': 'sgd', 'lr': 0.1, 'momentum':0.9, 'weight_decay': 5e-4, 'nesterov': True, 'LUT_lr':[(20, 0.01),(40, 0.006),(50, 0.0012),(60, 0.00024)]}
 net_optionsC = {'classifier_type': 'cosine', 'weight_generator_type': 'none', 'nKall': nKbase, 'nFeat':512, 'scale_cls': 10}
 networks['classifier'] = {'def_file': 'architectures/ClassifierWithFewShotGenerationModule.py', 'pretrained': None, 'opt': net_optionsC, 'optim_params': net_optim_paramsC}
 
