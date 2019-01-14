c274ecbd4b802ef18d9ae310acb33e97f9a75476
ZHANGyuan <fern35121321@outlook.com>
2019-01-14 02:42:20 +0100

    test experiments of CUB, change nTestBase to 0

--- config/cub_ResNet10CosineClassifier.py
+++ config/cub_ResNet10CosineClassifier.py
@@ -10,7 +10,7 @@ data_train_opt['nExemplars'] = 0
 data_train_opt['nTestNovel'] = 0
 data_train_opt['nTestBase'] = 32
 data_train_opt['batch_size'] = 32 #8
-data_train_opt['epoch_size'] = data_train_opt['batch_size'] * 250 #1000
+data_train_opt['epoch_size'] = data_train_opt['batch_size'] * 500 #1000
 
 
 data_test_opt = {}
@@ -30,10 +30,10 @@ config['data_test_opt'] = data_test_opt
 config['max_num_epochs'] = 4
 
 networks = {}
-net_optim_paramsF = {'optim_type': 'sgd', 'lr': 0.1, 'momentum':0.9, 'weight_decay': 5e-4, 'nesterov': True, 'LUT_lr':[(20, 0.02),(40, 0.006),(50, 0.0012),(60, 0.00024)]}
+net_optim_paramsF = {'optim_type': 'sgd', 'lr': 0.1, 'momentum':0.9, 'weight_decay': 5e-4, 'nesterov': True, 'LUT_lr':[(20, 0.05),(40, 0.006),(50, 0.0012),(60, 0.00024)]}
 networks['feat_model'] = {'def_file': 'architectures/ResNetFeat.py', 'pretrained': None, 'opt': {'userelu': False, 'restype': 'ResNet10'},  'optim_params': net_optim_paramsF}
 
-net_optim_paramsC = {'optim_type': 'sgd', 'lr': 0.1, 'momentum':0.9, 'weight_decay': 5e-4, 'nesterov': True, 'LUT_lr':[(20, 0.02),(40, 0.006),(50, 0.0012),(60, 0.00024)]}
+net_optim_paramsC = {'optim_type': 'sgd', 'lr': 0.1, 'momentum':0.9, 'weight_decay': 5e-4, 'nesterov': True, 'LUT_lr':[(20, 0.05),(40, 0.006),(50, 0.0012),(60, 0.00024)]}
 net_optionsC = {'classifier_type': 'cosine', 'weight_generator_type': 'none', 'nKall': nKbase, 'nFeat':512, 'scale_cls': 10}
 networks['classifier'] = {'def_file': 'architectures/ClassifierWithFewShotGenerationModule.py', 'pretrained': None, 'opt': net_optionsC, 'optim_params': net_optim_paramsC}
 
--- config/cub_ResNet10CosineClassifierGenWeightAttN1.py
+++ config/cub_ResNet10CosineClassifierGenWeightAttN1.py
@@ -12,7 +12,7 @@ data_train_opt['nExemplars'] = nExemplars
 data_train_opt['nTestNovel'] = nKnovel * 3
 data_train_opt['nTestBase'] = nKnovel * 3
 data_train_opt['batch_size'] = 32
-data_train_opt['epoch_size'] = data_train_opt['batch_size'] * 250
+data_train_opt['epoch_size'] = data_train_opt['batch_size'] * 500
 
 data_test_opt = {}
 data_test_opt['nKnovel'] = nKnovel
@@ -34,7 +34,7 @@ net_optionsF = {'userelu': False, 'restype': 'ResNet10'}
 pretrainedF = './experiments/cub_ResNet10CosineClassifier/feat_model_net_epoch*.best'
 networks['feat_model'] = {'def_file': 'architectures/ResNetFeat.py', 'pretrained': pretrainedF, 'opt': net_optionsF,  'optim_params': None }
 
-net_optim_paramsC = {'optim_type': 'sgd', 'lr': 0.1, 'momentum':0.9, 'weight_decay': 5e-4, 'nesterov': True, 'LUT_lr':[(10*2, 0.02),(20*2, 0.006),(25*2, 0.0012),(30*2, 0.00024)]}
+net_optim_paramsC = {'optim_type': 'sgd', 'lr': 0.1, 'momentum':0.9, 'weight_decay': 5e-4, 'nesterov': True, 'LUT_lr':[(10*2, 0.05),(20*2, 0.006),(25*2, 0.0012),(30*2, 0.00024)]}
 pretrainedC = './experiments/cub_ResNet10CosineClassifier/classifier_net_epoch*.best'
 net_optionsC = {'classifier_type': 'cosine', 'weight_generator_type': 'attention_based', 'nKall': nKbase, 'nFeat': 512, 'scale_cls': 10, 'scale_att': 10.0}
 networks['classifier'] = {'def_file': 'architectures/ClassifierWithFewShotGenerationModule.py', 'pretrained': pretrainedC, 'opt': net_optionsC, 'optim_params': net_optim_paramsC}
